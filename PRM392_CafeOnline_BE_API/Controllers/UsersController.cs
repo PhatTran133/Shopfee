cbcd389424e4fc790ae24134323d09090873bc42
Huy Tran <tranhoanghuy489@gmail.com>
2024-10-01 08:17:12 +0700

    delete old models

---
+++ PRM392_CafeOnline_BE_API/Controllers/UsersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PRM392_CafeOnline_BE_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+    }
+}
