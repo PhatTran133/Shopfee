ec36de7123ce235d5e64e49231fa40a283cecf59
tuankiet12012003 <kietdhtse172938@fpt.edu.vn>
2024-10-02 21:09:58 +0700

    show category name

---
+++ BussinessObjects/DTO/DrinkDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessObjects.DTO
+{
+    public class DrinkDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal? Price { get; set; }
+        public string CategoryName { get; set; }
+        public string? Size { get; set; }
+        public string? Image { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+
+}
