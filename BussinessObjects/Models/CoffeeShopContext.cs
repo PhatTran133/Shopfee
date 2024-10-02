782df96e15860e35f47e79d25b746f1719ac7b16
Huy Tran <tranhoanghuy489@gmail.com>
2024-10-03 01:32:57 +0700

    update id increment and fix api register

--- BussinessObjects/Models/CoffeeShopContext.cs
+++ BussinessObjects/Models/CoffeeShopContext.cs
@@ -33,7 +33,7 @@ namespace BussinessObjects.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-GBN7OP4\\HOANGHUY;Uid=sa;Pwd=12345;Database=CoffeeShop;TrustServerCertificate=True");
+                optionsBuilder.UseSqlServer("Server=(local);Uid=sa;Pwd=12345;Database=CoffeeShop;TrustServerCertificate=True");
             }
         }
 
