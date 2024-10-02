0ece08e62d131372a588e0e4b7f8ce67d8583be2
nam127 <buithanhnam9109@gmail.com>
2024-10-02 20:12:32 +0700

    feat: get user profile by userId

--- PRM392_CafeOnline_BE_API/Controllers/UsersController.cs
+++ PRM392_CafeOnline_BE_API/Controllers/UsersController.cs
@@ -1,11 +1,47 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DataAccess.DTO;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Repositories.Interface;
 
 namespace PRM392_CafeOnline_BE_API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/users")]
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly IUserRepository _userRepository;
+        public UsersController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> GetUser(int userId)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(userId)))
+            {
+                return BadRequest();
+            }
+
+            var user = await _userRepository.GetTblUser(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var userResponse = new UserDTO
+            {
+                Id = userId,
+                Address = user.Address,
+                CreatedDate = user.CreatedDate,
+                Email = user.Email,
+                Password = user.Password,
+                Phone = user.Phone,
+                UpdatedDate = user.UpdatedDate,
+                Username = user.Username,
+            };
+
+            return Ok(userResponse);
+        }
     }
 }
--- Repositories/TblUserRepository.cs
+++ Repositories/TblUserRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BussinessObjects.Models;
+using Microsoft.EntityFrameworkCore;
+using Repositories.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,16 @@ using System.Threading.Tasks;
 
 namespace Repositories
 {
-    public class TblUserRepository
+    public class TblUserRepository : IUserRepository
     {
+        private readonly CoffeeShopContext _context;
+        public TblUserRepository(CoffeeShopContext context)
+        {
+            _context = context;
+        }
+        public async Task<TblUser> GetTblUser(int id)
+        {
+            return await _context.TblUsers.FirstOrDefaultAsync(_context => _context.Id == id) ?? throw new Exception("Not found user");
+        }
     }
 }
