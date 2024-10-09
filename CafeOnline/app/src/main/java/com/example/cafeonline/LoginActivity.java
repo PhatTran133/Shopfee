024a2b757e1b6968945309968d9c3dc150df2216
Phat Tran <tranphat762003@gmail.com>
2024-10-09 15:13:57 +0700

    done call api login register - have not tested

--- CafeOnline/app/src/main/java/com/example/cafeonline/LoginActivity.java
+++ CafeOnline/app/src/main/java/com/example/cafeonline/LoginActivity.java
@@ -38,12 +38,13 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // Initialize views
+
         edtEmail = findViewById(R.id.edt_email);
         edtPassword = findViewById(R.id.edt_password);
         btnLogin = findViewById(R.id.btn_login);
         btnForgotPassword = findViewById(R.id.tv_forgot_password);
-        // Set up click listener for login button
+
+
         btnLogin.setOnClickListener(v -> performLogin());
         btnForgotPassword.setOnClickListener(v -> {
             Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
@@ -57,19 +58,15 @@ public class LoginActivity extends AppCompatActivity {
         String email = edtEmail.getText().toString().trim();
         String password = edtPassword.getText().toString().trim();
 
-        // Check if fields are empty
+
         if (email.isEmpty() || password.isEmpty()) {
             Toast.makeText(this, "Please enter both email and password", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        // Create login request object
+
         LoginRequest loginRequest = new LoginRequest(email, password);
-
-        // Create AuthService instance
         UserApiService authService = ApiService.createService(UserApiService.class);
-
-        // Make API call
         Call<ApiResponse<UserResponse>> call = authService.loginUser(loginRequest);
         call.enqueue(new Callback<ApiResponse<UserResponse>>() {
             @Override
@@ -79,21 +76,20 @@ public class LoginActivity extends AppCompatActivity {
                     ApiResponse<UserResponse> apiResponse = response.body();
 
                     if ("200".equals(apiResponse.getValue().getStatus())) {
-                        // Login successful
+
                         Toast.makeText(LoginActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
-                        System.out.println("Login Successfully");
+
                     } else {
-                        // Login failed with specific message
+
                         Toast.makeText(LoginActivity.this, "Login Failed: " + apiResponse.getValue().getMessage(), Toast.LENGTH_SHORT).show();
-                    }   System.out.println("Login Failed");
+                    }
                 } else {
-                    // Handle error case
+
                     Toast.makeText(LoginActivity.this, "Login Failed: Invalid response", Toast.LENGTH_SHORT).show();
                 }
             }
             @Override
             public void onFailure(Call<ApiResponse<UserResponse>> call, Throwable t) {
-                System.out.println("Failure occurred: " + t.getMessage());
                 Toast.makeText(LoginActivity.this, "Login Failed: " + t.getMessage(), Toast.LENGTH_SHORT).show();
 
             }
--- CafeOnline/app/src/main/java/com/example/cafeonline/api/UserApiService.java
+++ CafeOnline/app/src/main/java/com/example/cafeonline/api/UserApiService.java
@@ -2,6 +2,7 @@ package com.example.cafeonline.api;
 
 import com.example.cafeonline.model.request.ForgotPasswordRequest;
 import com.example.cafeonline.model.request.LoginRequest;
+import com.example.cafeonline.model.request.RegisterRequest;
 import com.example.cafeonline.model.request.ResetPasswordRequest;
 import com.example.cafeonline.model.request.UserProfileRequest;
 import com.example.cafeonline.model.request.VerifyCodeRequest;
@@ -14,8 +15,16 @@ import retrofit2.http.POST;
 
 public interface UserApiService {
 
-    @POST("api/v1/login")
+    //PHAT
+    @POST("api/Auth/login")
     Call<ApiResponse<UserResponse>> loginUser(@Body LoginRequest loginRequest);
+    @POST("api/Auth/register")
+    Call<ApiResponse<String>> registerUser(@Body RegisterRequest registerRequest);
+    @POST("")//Check lại theo BE
+    Call<ApiResponse<String>> verifyCodeForRegister(@Body VerifyCodeRequest verifyCodeRequest);
+
+
+    //DƯƠNG
     @POST("api/authentication/forgot-password")
     Call<ApiResponse<UserResponse>> forgotPassword(@Body ForgotPasswordRequest forgotPasswordRequest);
     @POST("api/authentication/verify-reset-code")
