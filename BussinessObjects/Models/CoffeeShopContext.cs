b787cf1f65668b1cb9120924157c8cda50e34e75
tuankiet12012003 <kietdhtse172938@fpt.edu.vn>
2024-10-13 15:21:37 +0700

    deploy

--- BussinessObjects/Models/CoffeeShopContext.cs
+++ BussinessObjects/Models/CoffeeShopContext.cs
@@ -37,8 +37,10 @@ namespace BussinessObjects.Models
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 //optionsBuilder.UseSqlServer("Server=(local);Uid=sa;Pwd=12345;Database=CoffeeShop;TrustServerCertificate=True");
-                //optionsBuilder.UseSqlServer("Server=database-1.cd82m0owep6n.ap-southeast-2.rds.amazonaws.com,1433;Database = CoffeeShop;Uid=admin; Pwd = Helloworld12345; Trusted_Connection = True");
-                optionsBuilder.UseSqlServer("Server=(local);Database=CoffeeShop;Trusted_Connection=True;");
+
+                //Dòng này để chạy DB deploy
+                optionsBuilder.UseSqlServer("Server=database-1.cd82m0owep6n.ap-southeast-2.rds.amazonaws.com,1433;Database = CoffeeShop;Uid=admin; Pwd = Helloworld12345; Trusted_Connection = True;integrated security=false");
+                //optionsBuilder.UseSqlServer("Server=(local);Database=CoffeeShop;Trusted_Connection=True;");
             }
         }
 
